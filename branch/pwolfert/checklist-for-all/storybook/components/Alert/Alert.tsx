664861187f050fb6297a1483d1b35c78197dd60d
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2022-09-27 18:39:24 -0700

    Updates

---
+++ branch/pwolfert/checklist-for-all/storybook/components/Alert/Alert.tsx
@@ -0,0 +1,172 @@
+import React, { useRef } from 'react';
+import classNames from 'classnames';
+import mergeRefs from '../utilities/mergeRefs';
+import uniqueId from 'lodash/uniqueId';
+import useAutofocus from '../utilities/useAutoFocus';
+import useAlertAnalytics from './useAlertAnalytics';
+import { InfoCircleIcon, AlertCircleIcon, WarningIcon, CheckCircleIcon } from '../Icons';
+import { t } from '../i18n';
+
+export type AlertHeadingLevel = '1' | '2' | '3' | '4' | '5' | '6';
+export type AlertRole = 'alert' | 'alertdialog' | 'region' | 'status';
+export type AlertVariation = 'error' | 'warn' | 'success';
+export type AlertWeight = 'lightweight';
+
+export interface BaseAlertProps {
+  /**
+   * Access a reference to the `alert` `div` element
+   */
+  alertRef?: (...args: any[]) => any;
+  /**
+   * Analytics events tracking is enabled by default. Set this value to `false` to disable tracking for this component instance.
+   */
+  analytics?: boolean;
+  /**
+   * An override for the dynamic content sent to analytics services. By default this content comes from the heading.
+   *
+   * In cases where this component’s heading may contain **sensitive information**, use this prop to override what is sent to analytics.
+   */
+  analyticsLabelOverride?: string;
+  /**
+   * Sets the focus on Alert during the first mount
+   */
+  autoFocus?: boolean;
+  /**
+   * The alert's body content
+   */
+  children?: React.ReactNode;
+  className?: string;
+  /**
+   * Text for the alert heading
+   */
+  heading?: string;
+  /**
+   * Optional id used to link the `aria-labelledby` attribute to the heading. If not provided, a unique id will be automatically generated and used.
+   */
+  headingId?: string;
+  /**
+   * Heading type to override default `<h2>`.
+   */
+  headingLevel?: AlertHeadingLevel;
+  /**
+   * Boolean to hide the `Alert` icon
+   */
+  hideIcon?: boolean;
+  /**
+   * ARIA `role`, defaults to 'region'
+   */
+  role?: AlertRole;
+  /**
+   * A string corresponding to the `Alert` weight classes (`lightweight`)
+   */
+  weight?: AlertWeight;
+  /**
+   * A string corresponding to the `Alert` variation classes (`error`, `warn`, `success`)
+   */
+  variation?: AlertVariation;
+}
+
+export type AlertProps = BaseAlertProps &
+  Omit<React.ComponentPropsWithRef<'div'>, keyof BaseAlertProps>;
+
+export const Alert: React.FC<AlertProps> = (props: AlertProps) => {
+  const { headingRef, bodyRef } = useAlertAnalytics(props);
+  const focusRef = useAutofocus(props.autoFocus);
+  const headingId = useRef(props.headingId ?? uniqueId('alert_')).current;
+  const a11yLabelId = useRef(uniqueId('alert_a11y_label_')).current;
+
+  if (process.env.NODE_ENV !== 'production') {
+    if (!props.heading && !props.children) {
+      console.warn(
+        `Empty <Alert> components are not allowed, please use the 'heading' prop or include children.`
+      );
+    }
+  }
+
+  const {
+    children,
+    className,
+    autoFocus,
+    heading,
+    headingId: _headingId,
+    headingLevel,
+    hideIcon,
+    alertRef,
+    role,
+    variation,
+    weight,
+    analytics,
+    analyticsLabelOverride,
+    ...alertProps
+  } = props;
+
+  let headingElement;
+  if (heading) {
+    const Heading = `h${headingLevel}` as const;
+    headingElement = <Heading className="ds-c-alert__heading">{heading}</Heading>;
+  }
+
+  const classes = classNames(
+    'ds-c-alert',
+    hideIcon && 'ds-c-alert--hide-icon',
+    variation && `ds-c-alert--${variation}`,
+    weight && `ds-c-alert--${weight}`,
+    className
+  );
+
+  const a11yLabel = (
+    <span className="ds-c-alert__a11y-label ds-u-visibility--screen-reader" id={a11yLabelId}>
+      {t(`alert.${variation ?? 'defaultLabel'}`)}:{' '}
+    </span>
+  );
+
+  // getting proper icon for alert variation
+  function getIcon() {
+    const iconClass = 'ds-c-alert__icon';
+    if (hideIcon) {
+      return null;
+    }
+
+    switch (variation) {
+      case 'error':
+        return <AlertCircleIcon className={iconClass} />;
+      case 'success':
+        return <CheckCircleIcon className={iconClass} />;
+      case 'warn':
+        return <WarningIcon className={iconClass} />;
+      default:
+        return <InfoCircleIcon className={iconClass} />;
+    }
+  }
+
+  return (
+    <div
+      className={classes}
+      ref={mergeRefs([alertRef, focusRef])}
+      tabIndex={alertRef || autoFocus ? -1 : null}
+      role={role}
+      aria-labelledby={heading ? headingId : a11yLabelId}
+      {...alertProps}
+    >
+      {getIcon()}
+      <div className="ds-c-alert__body" id={headingId} ref={bodyRef}>
+        {heading ? (
+          <div className="ds-c-alert__header ds-c-alert__heading" ref={headingRef}>
+            {a11yLabel}
+            {headingElement}
+          </div>
+        ) : (
+          a11yLabel
+        )}
+        {children}
+      </div>
+    </div>
+  );
+};
+
+Alert.defaultProps = {
+  role: 'region',
+  headingLevel: '2',
+};
+
+export default Alert;
---
+++ branch/pwolfert/checklist-for-all/storybook/components/Drawer/DrawerToggle.test.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import DrawerToggle, { DrawerToggleProps } from './DrawerToggle';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+const defaultProps = {
+  children: 'content',
+  drawerOpen: false,
+  inline: false,
+  showDrawer: jest.fn(),
+};
+
+function renderDrawerToggle(props: Partial<DrawerToggleProps> = {}) {
+  return render(
+    <DrawerToggle {...defaultProps} {...props}>
+      content
+    </DrawerToggle>
+  );
+}
+describe('DrawerToggle', () => {
+  beforeEach(() => {
+    defaultProps.showDrawer.mockClear();
+  });
+
+  it('renders a button', () => {
+    const { asFragment } = renderDrawerToggle();
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('calls showDrawer() on toggle click', () => {
+    renderDrawerToggle();
+    userEvent.click(screen.getByRole('button'));
+    expect(defaultProps.showDrawer).toHaveBeenCalled();
+  });
+
+  it('applies inline display style via inline prop', () => {
+    renderDrawerToggle({ inline: true });
+    const toggle = screen.getByRole('button');
+    expect(toggle.classList.contains('ds-c-drawer__toggle--inline')).toBe(true);
+  });
+
+  it('applies custom class via className prop', () => {
+    const className = 'test-class';
+    renderDrawerToggle({ className });
+    const toggle = screen.getByRole('button');
+    expect(toggle.classList).toContain(className);
+  });
+
+  it('passes through extra props', () => {
+    const ariaLabel = 'test';
+    renderDrawerToggle({ 'aria-label': ariaLabel });
+    const toggle = screen.getByRole('button');
+    expect(toggle.getAttribute('aria-label')).toBe(ariaLabel);
+  });
+
+  it('focuses button when drawer goes from open to closed', () => {
+    const { rerender } = renderDrawerToggle({ drawerOpen: true });
+    rerender(
+      <DrawerToggle {...defaultProps} drawerOpen={false}>
+        content
+      </DrawerToggle>
+    );
+    const toggle = screen.getByRole('button');
+    expect(toggle).toEqual(document.activeElement);
+  });
+
+  it('does not focus button when drawer in initialized to close', () => {
+    renderDrawerToggle({ drawerOpen: false });
+    const toggle = screen.getByRole('button');
+    expect(toggle).not.toEqual(document.activeElement);
+  });
+});
