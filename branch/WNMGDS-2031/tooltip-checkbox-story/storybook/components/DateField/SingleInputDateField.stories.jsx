8849798f952676d54696d286cba0eb3cde11258b
zarahzachz <sarah.sachs@gmail.com>
2022-11-03 11:38:20 -0400

    Updates

---
+++ branch/WNMGDS-2031/tooltip-checkbox-story/storybook/components/DateField/SingleInputDateField.stories.jsx
@@ -0,0 +1,59 @@
+import SingleInputDateField from './SingleInputDateField';
+import { action } from '@storybook/addon-actions';
+import { useState } from 'react';
+
+export default {
+  title: 'Components/SingleInputDateField',
+  component: SingleInputDateField,
+  argTypes: {
+    errorMessage: {
+      control: { type: 'text' },
+    },
+    errorPlacement: {
+      defaultValue: 'top',
+      control: {
+        type: 'radio',
+      },
+      options: ['top', 'bottom'],
+    },
+    hint: {
+      control: { type: 'text' },
+    },
+    label: {
+      control: { type: 'text' },
+    },
+    requirementLabel: {
+      control: { type: 'text' },
+    },
+  },
+  args: {
+    label: 'Birthday',
+    hint: 'Please enter your birthday',
+    name: 'single-input-date-field',
+  },
+};
+
+const Template = ({ ...args }) => {
+  const [dateString, setDateString] = useState('');
+  const onChange = (...params) => {
+    action('onChange')(...params);
+    setDateString(...params);
+  };
+  return <SingleInputDateField {...args} value={dateString} onChange={onChange} />;
+};
+
+export const Default = Template.bind({});
+
+export const WithPicker = Template.bind({});
+WithPicker.args = {
+  label: 'What day did you move?',
+  hint: 'This date should be within the past 60 days in order to qualify',
+  fromYear: new Date().getFullYear(),
+  toDate: new Date(),
+};
+
+export const WithError = Template.bind({});
+WithError.args = {
+  ...WithPicker.args,
+  errorMessage: 'Example error message',
+};
