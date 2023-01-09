1b9029d70e454dfe73f6a59d30a2fc89b7b1943e
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2023-01-09 14:18:56 -0800

    Updates

---
+++ branch/pwolfert/svg-swatches/storybook/components/Spinner/Spinner.stories.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import Spinner from './Spinner';
+
+export default {
+  title: 'Components/Spinner',
+  component: Spinner,
+  argTypes: {
+    size: {
+      control: 'radio',
+      options: ['small', 'big'],
+    },
+    inversed: {
+      control: 'radio',
+      options: [true, false],
+    },
+    filled: {
+      control: 'radio',
+      options: [true, false],
+    },
+  },
+  args: {
+    inversed: false,
+    filled: false,
+  },
+};
+
+const Template = ({ data, ...args }) => <Spinner {...args} />;
+
+export const DefaultSpinner = Template.bind({});
+
+export const SmallSpinner = Template.bind({});
+SmallSpinner.args = {
+  size: 'small',
+};
+
+export const BigSpinner = Template.bind({});
+BigSpinner.args = {
+  size: 'big',
+};
+
+export const FilledSpinner = Template.bind({});
+FilledSpinner.args = {
+  filled: true,
+};
+FilledSpinner.parameters = {
+  baseInverse: true,
+};
+
+export const InverseFilledSpinner = Template.bind({});
+InverseFilledSpinner.args = {
+  inversed: true,
+  filled: true,
+};
