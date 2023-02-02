25ede21a28c1505d595f510615e024645080aded
zarahzachz <sarah.sachs@gmail.com>
2023-02-02 13:34:44 -0500

    Updates

---
+++ branch/WNMGDS-2183/css-reset/storybook/components/ChoiceList/Choice.stories.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Choice from './Choice';
+
+export default {
+  title: 'Components/Choice',
+  component: Choice,
+  args: {
+    name: 'attestation',
+    value: 'attestation',
+    type: 'checkbox',
+    label: 'I agree to the above terms and conditions',
+    hint: 'This is some additional hint text',
+    errorMessage: 'You must agree to the terms and conditions before continuing',
+    defaultChecked: false,
+  },
+  argTypes: {
+    checkedChildren: { control: 'text' },
+    uncheckedChildren: { control: 'text' },
+  },
+};
+
+const Template = (args) => <Choice {...args} />;
+
+export const Default = Template.bind({});
---
+++ branch/WNMGDS-2183/css-reset/storybook/components/HelpDrawer/HelpDrawer.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Drawer, { DrawerProps } from '../Drawer/Drawer';
+import classNames from 'classnames';
+import useHelpDrawerAnalytics from './useHelpDrawerAnalytics';
+import { AnalyticsOverrideProps } from '../analytics';
+
+export interface HelpDrawerProps extends DrawerProps, AnalyticsOverrideProps {}
+
+export const HelpDrawer = (props: HelpDrawerProps) => {
+  const {
+    analytics,
+    analyticsLabelOverride,
+    analyticsEventTypeOverride,
+    children,
+    className,
+    ...others
+  } = props;
+  const headingRef = useHelpDrawerAnalytics(props);
+
+  return (
+    <Drawer
+      className={classNames(className, 'ds-c-help-drawer')}
+      headingRef={headingRef}
+      {...others}
+    >
+      {children}
+    </Drawer>
+  );
+};
+
+export default HelpDrawer;
