c3ceb1a1e029fdf721e444136d24682e4441379e
Sarah <zarahzachz@users.noreply.github.com>
2023-03-03 11:24:52 -0500

    Updates

---
+++ branch/WNMGDS-2171/css-reset-docs/storybook/components/HelpDrawer/useHelpDrawerAnalytics.ts
@@ -0,0 +1,58 @@
+import {
+  defaultAnalyticsFunction,
+  EventCategory,
+  EventType,
+  useAnalyticsContent,
+  eventExtensionText,
+} from '../analytics';
+import { HelpDrawerProps } from './HelpDrawer';
+import { helpDrawerSendsAnalytics } from '../flags';
+
+export default function useHelpDrawerAnalytics({
+  analytics,
+  analyticsLabelOverride,
+  onAnalyticsEvent = defaultAnalyticsFunction,
+}: HelpDrawerProps) {
+  function sendHelpDrawerEvent(
+    content: string | undefined,
+    eventAttributes: { event_name: string; event_action: string }
+  ) {
+    if (analytics !== true && (!helpDrawerSendsAnalytics() || analytics === false)) {
+      return;
+    }
+
+    const eventHeadingText = analyticsLabelOverride ?? content;
+
+    if (!eventHeadingText) {
+      console.error('No content found for Dialog analytics event');
+      return;
+    }
+
+    onAnalyticsEvent({
+      event_type: EventType.UI_INTERACTION,
+      event_category: EventCategory.UI_COMPONENTS,
+      event_label: eventHeadingText,
+      event_extension: eventExtensionText,
+      heading: eventHeadingText,
+      ...eventAttributes,
+    });
+  }
+
+  const [headingRef] = useAnalyticsContent({
+    componentName: 'Dialog',
+    onMount: (content: string | undefined) => {
+      sendHelpDrawerEvent(content, {
+        event_name: 'help_drawer_opened',
+        event_action: 'opened help drawer',
+      });
+    },
+    onUnmount: (content: string | undefined) => {
+      sendHelpDrawerEvent(content, {
+        event_name: 'help_drawer_closed',
+        event_action: 'closed help drawer',
+      });
+    },
+  });
+
+  return headingRef;
+}
---
+++ branch/WNMGDS-2171/css-reset-docs/storybook/components/Table/TableCell.test.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import TableCell from './TableCell';
+import TableRow from './TableRow';
+import TableHead from './TableHead';
+
+const makeTableCells = (customProps = {}) => {
+  const children = (
+    <>
+      <TableHead>
+        <TableRow>
+          <TableCell>Column 1</TableCell>
+          <TableCell>Column 2</TableCell>
+        </TableRow>
+      </TableHead>
+      <tbody>
+        <TableRow>
+          <TableCell>Column a</TableCell>
+          <TableCell>Column b</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell align="right">Column a2</TableCell>
+          <TableCell component="th">Column b2</TableCell>
+        </TableRow>
+      </tbody>
+    </>
+  );
+  render(<table {...customProps}>{children}</table>);
+};
+
+describe('TableCell', function () {
+  it('renders TableCell components', () => {
+    makeTableCells();
+    const tds = screen.getAllByRole('cell');
+    expect(tds).toHaveLength(3);
+    expect(tds).toMatchSnapshot();
+  });
+
+  it('sets alignment default to left', () => {
+    makeTableCells();
+    expect(screen.getByText('Column a')).toHaveClass('ds-u-text-align--left');
+  });
+
+  it('accepts alternate align options', () => {
+    makeTableCells();
+    expect(screen.getByText('Column a2')).toHaveClass('ds-u-text-align--right');
+  });
+
+  it('accepts alternate component roles', () => {
+    makeTableCells();
+    expect(screen.getAllByRole('rowheader')).toHaveLength(1);
+  });
+
+  it('renders TableCell components as <th> elements when inside a TableHeader', () => {
+    makeTableCells();
+    const tds = screen.getAllByRole('columnheader');
+    expect(tds).toHaveLength(2);
+  });
+
+  it('applies col scope when parent is TableHead', () => {
+    makeTableCells();
+    const cell = screen.getByText('Column 1');
+    expect(cell).toHaveAttribute('scope', 'col');
+  });
+});
