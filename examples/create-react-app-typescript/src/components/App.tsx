3b4a0a90edac28e143309f9dc48065122ef4ce7f
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2023-03-09 16:28:04 -0800

    Modify an example to use WebPack 4 and Postcss 7 to troubleshoot

--- examples/create-react-app-typescript/src/components/App.tsx
+++ examples/create-react-app-typescript/src/components/App.tsx
@@ -1,27 +1,27 @@
-import { Badge } from '@cmsgov/design-system';
+// import { Badge } from '@cmsgov/design-system';
 import React, { useState } from 'react';
-import AccordionExample from './Examples/AccordionExample';
-import AlertExample from './Examples/AlertExample';
-import AutocompleteExample from './Examples/AutocompleteExample';
-import BadgeExample from './Examples/BadgeExample';
-import ButtonExample from './Examples/ButtonExample';
-import ChoiceListExample from './Examples/ChoiceListExample';
-import DateFieldExample from './Examples/DateFieldExample';
-import DropdownExample from './Examples/DropdownExample';
-import FilterChipExample from './Examples/FilterChipExample';
-import FormLabelExample from './Examples/FormLabelExample';
-import HelpDrawerExample from './Examples/HelpDrawerExample';
-import MaskedFieldExample from './Examples/MaskedFieldExample';
-import ModalDialogExample from './Examples/ModalDialogExample';
-import MonthPickerExample from './Examples/MonthPickerExample';
-import PaginationExample from './Examples/PaginationExample';
-import SpinnerExample from './Examples/SpinnerExample';
-import TableExample from './Examples/TableExample';
-import TabsExample from './Examples/TabsExample';
-import TextFieldExample from './Examples/TextFieldExample';
-import TooltipExample from './Examples/TooltipExample';
-import UsaBannerExample from './Examples/UsaBannerExample';
-import VerticalNavigationExample from './Examples/VerticalNavigationExample';
+// import AccordionExample from './Examples/AccordionExample';
+// import AlertExample from './Examples/AlertExample';
+// import AutocompleteExample from './Examples/AutocompleteExample';
+// import BadgeExample from './Examples/BadgeExample';
+// import ButtonExample from './Examples/ButtonExample';
+// import ChoiceListExample from './Examples/ChoiceListExample';
+// import DateFieldExample from './Examples/DateFieldExample';
+// import DropdownExample from './Examples/DropdownExample';
+// import FilterChipExample from './Examples/FilterChipExample';
+// import FormLabelExample from './Examples/FormLabelExample';
+// import HelpDrawerExample from './Examples/HelpDrawerExample';
+// import MaskedFieldExample from './Examples/MaskedFieldExample';
+// import ModalDialogExample from './Examples/ModalDialogExample';
+// import MonthPickerExample from './Examples/MonthPickerExample';
+// import PaginationExample from './Examples/PaginationExample';
+// import SpinnerExample from './Examples/SpinnerExample';
+// import TableExample from './Examples/TableExample';
+// import TabsExample from './Examples/TabsExample';
+// import TextFieldExample from './Examples/TextFieldExample';
+// import TooltipExample from './Examples/TooltipExample';
+// import UsaBannerExample from './Examples/UsaBannerExample';
+// import VerticalNavigationExample from './Examples/VerticalNavigationExample';
 import usflag from '../images/us_flag_small.png';
 
 function App() {
@@ -34,15 +34,15 @@ function App() {
           Hello, world!
         </h1>
         <div className="ds-u-text-align--center">
-          <Badge variation="info" size="big">
+          {/* <Badge variation="info" size="big">
             <img className="c-usa-banner__header-flag" src={usflag} alt="U.S. flag" />
             &nbsp;CMS Design system
-          </Badge>
+          </Badge> */}
         </div>
       </header>
 
       <div className="ds-l-container">
-        <AccordionExample />
+        {/* <AccordionExample />
         <AlertExample />
         <AutocompleteExample />
         <BadgeExample />
@@ -63,7 +63,7 @@ function App() {
         <TabsExample />
         <TooltipExample />
         <UsaBannerExample />
-        <VerticalNavigationExample />
+        <VerticalNavigationExample /> */}
       </div>
     </div>
   );
